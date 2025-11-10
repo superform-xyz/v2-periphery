7fdfdc2285bfc31b9e7fbb3464a1fee653e8f4b5
Tamara Ringas <tamriasdev@gmail.com>
2025-11-10 21:05:11 +0200

    chore: update natspec

--- src/interfaces/IHookExecutionData.sol
+++ src/interfaces/IHookExecutionData.sol
@@ -5,10 +5,12 @@ interface IHookExecutionData {
     /// @notice Data required for executing hooks with Merkle proof verification.
     /// @param hooks Array of addresses of hooks to execute.
     /// @param data Array of arbitrary data to pass to each hook.
+    /// @param expectedAssetsOrSharesOut Array of expected assets or shares out for each hook.
     /// @param merkleProofs Double array of Merkle proofs verifying each hook's allowed targets.
     struct HookExecutionData {
         address[] hooks;
         bytes[] data;
+        uint256[] expectedAssetsOrSharesOut;
         bytes32[][] merkleProofs;
     }
 }
