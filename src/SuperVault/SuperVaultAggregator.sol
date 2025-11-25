3d110bf3ae8ed35be943d324df7d2690a30ff8f6
Tamara Ringas <tamriasdev@gmail.com>
2025-11-25 11:44:37 +0200

    feat: prevent main manager from being added as a secondary manager

--- src/SuperVault/SuperVaultAggregator.sol
+++ src/SuperVault/SuperVaultAggregator.sol
@@ -57,7 +57,7 @@ contract SuperVaultAggregator is ISuperVaultAggregator {
     EnumerableSet.AddressSet private _superVaultStrategies;
     EnumerableSet.AddressSet private _superVaultEscrows;
 
-    // Constant for basis points precision (100% = 10,000 bps) 
+    // Constant for basis points precision (100% = 10,000 bps)
     uint256 private constant BPS_PRECISION = 10_000;
 
     // Maximum performance fee allowed (51%)
@@ -199,6 +199,9 @@ contract SuperVaultAggregator is ISuperVaultAggregator {
 
         uint256 secondaryLen = params.secondaryManagers.length;
         for (uint256 i; i < secondaryLen; ++i) {
+            // Check if manager is already the primary manager
+            if (_strategyData[strategy].mainManager == params.secondaryManagers[i]) revert MANAGER_ALREADY_EXISTS();
+            
             _strategyData[strategy].secondaryManagers.add(params.secondaryManagers[i]);
         }
         if (_strategyData[strategy].secondaryManagers.length() > MAX_SECONDARY_MANAGERS) {
